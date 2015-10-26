bf8b6395b3fd5b0b7dfd55e9bf3eb99fbe98e055
bitfurry <sahilkharb@iitj.ac.in>
2015-10-26 15:21:22 +0530

    next generation implemented now able to get next generation

--- lib/generation.rb
+++ lib/generation.rb
@@ -2,7 +2,7 @@ class Generation
   def initialize(curr_locations= nil)
     @curr_locations = curr_locations
     @next_gen_locations = []
-    @neighbours = []
+    @neighbours = Set.new
   end
 
   def curr_locations
@@ -11,12 +11,12 @@ class Generation
 
   def next_gen
     if @curr_locations == nil
-      nil
+      return nil
     else
       @curr_locations.each do |loc |
         cell = Cell.new("ALIVE",loc[0],loc[1])
         tmp_neighbours = cell.neighbours
-        @neighbours << tmp_neighbours
+        @neighbours.add(tmp_neighbours)
         count = 0
         tmp_neighbours.each do |tmp|
           if @curr_locations.include? tmp
@@ -27,17 +27,18 @@ class Generation
           @next_gen_locations << loc
         end
       end
-      @neighbours = @neighbours.flatten(1).uniq
+    end
 
-      @neighbours.each do |loc|
-        cell = Cell.new("ALIVE",loc[0],loc[1])
-        @neighbours << cell.neighbours
-      end
+    tmp_neigh = @neighbours.to_a.flatten(1).uniq
+    tmp_neigh.each do |loc|
+       cell = Cell.new("ALIVE",loc[0],loc[1])
+        tmp_neighbours = cell.neighbours
+        @neighbours.add(tmp_neighbours)
+    end
 
-      @neighbours = @neighbours.flatten(1).uniq
-
-      @neighbours.each do |loc|
-        if !@curr_locations.include? loc
+    @neighbours =  @neighbours.to_a.flatten(1).uniq
+    @neighbours.each do |loc|
+      if !@curr_locations.include? loc
         cell = Cell.new("ALIVE",loc[0],loc[1])
         tmp_neighbours = cell.neighbours
         count = 0
@@ -46,13 +47,12 @@ class Generation
             count += 1
           end
         end
-        if  count == 3
+        if count == 3
           @next_gen_locations << loc
         end
-        end
       end
-      @next_gen_locations
-    end
+      end
+    @next_gen_locations
   end
 
 end
