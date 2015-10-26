ae025d4978a5f4db64241b86d6fa7c14153522a7
bitfurry <sahilkharb@iitj.ac.in>
2015-10-26 14:55:21 +0530

    next generation locations implementing

--- lib/generation.rb
+++ lib/generation.rb
@@ -2,6 +2,7 @@ class Generation
   def initialize(curr_locations= nil)
     @curr_locations = curr_locations
     @next_gen_locations = []
+    @neighbours = []
   end
 
   def curr_locations
@@ -14,9 +15,10 @@ class Generation
     else
       @curr_locations.each do |loc |
         cell = Cell.new("ALIVE",loc[0],loc[1])
-        neighbours = cell.neighbours
+        tmp_neighbours = cell.neighbours
+        @neighbours << tmp_neighbours
         count = 0
-        neighbours.each do |tmp|
+        tmp_neighbours.each do |tmp|
           if @curr_locations.include? tmp
             count += 1
           end
@@ -25,7 +27,29 @@ class Generation
           @next_gen_locations << loc
         end
       end
+      @neighbours = @neighbours.flatten.uniq
 
+      @neighbours.each do |loc|
+        cell = Cell.new("ALIVE",loc[0],loc[1])
+        @neighbours << cell.neighbours
+      end
+      @neighbours = @neighbours.flatten.uniq
+
+      @neighbours.each do |loc|
+        if !@curr_locations.include? loc
+        cell = Cell.new("ALIVE",loc[0],loc[1])
+        tmp_neighbours = cell.neighbours
+        count = 0
+        tmp_neighbours.each do |tmp|
+          if @curr_locations.include? tmp
+            count += 1
+          end
+        end
+        if  count == 3
+          @next_gen_locations << loc
+        end
+        end
+      end
       @next_gen_locations
     end
   end
